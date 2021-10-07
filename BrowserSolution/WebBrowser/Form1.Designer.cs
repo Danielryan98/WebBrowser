2dfbe1b7972d71806a53b092fbad3e962537d9bf
Daniel Ryan <ryandaniel625@gmail.com>
2021-10-07 10:16:07 +0100

    Forward, backwards, and refresh working, though forward and backward being a little funny

--- BrowserSolution/WebBrowser/Form1.Designer.cs
+++ BrowserSolution/WebBrowser/Form1.Designer.cs
@@ -29,13 +29,95 @@ namespace WebBrowser
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.searchBar = new System.Windows.Forms.TextBox();
+            this.btnBack = new System.Windows.Forms.Button();
+            this.btnForward = new System.Windows.Forms.Button();
+            this.btnRefresh = new System.Windows.Forms.Button();
+            this.btnSearch = new System.Windows.Forms.Button();
+            this.htmlTextBox = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // searchBar
+            // 
+            this.searchBar.Location = new System.Drawing.Point(268, 9);
+            this.searchBar.Name = "searchBar";
+            this.searchBar.Size = new System.Drawing.Size(400, 20);
+            this.searchBar.TabIndex = 1;
+            // 
+            // btnBack
+            // 
+            this.btnBack.Location = new System.Drawing.Point(66, 7);
+            this.btnBack.Name = "btnBack";
+            this.btnBack.Size = new System.Drawing.Size(75, 23);
+            this.btnBack.TabIndex = 2;
+            this.btnBack.Text = "<";
+            this.btnBack.UseVisualStyleBackColor = true;
+            this.btnBack.Click += new System.EventHandler(this.Back);
+            // 
+            // btnForward
+            // 
+            this.btnForward.Location = new System.Drawing.Point(147, 7);
+            this.btnForward.Name = "btnForward";
+            this.btnForward.Size = new System.Drawing.Size(75, 23);
+            this.btnForward.TabIndex = 3;
+            this.btnForward.Text = ">";
+            this.btnForward.UseVisualStyleBackColor = true;
+            this.btnForward.Click += new System.EventHandler(this.Forward);
+            // 
+            // btnRefresh
+            // 
+            this.btnRefresh.Location = new System.Drawing.Point(228, 7);
+            this.btnRefresh.Name = "btnRefresh";
+            this.btnRefresh.Size = new System.Drawing.Size(34, 23);
+            this.btnRefresh.TabIndex = 4;
+            this.btnRefresh.Text = "R";
+            this.btnRefresh.UseVisualStyleBackColor = true;
+            this.btnRefresh.Click += new System.EventHandler(this.Refresh);
+            // 
+            // btnSearch
+            // 
+            this.btnSearch.Location = new System.Drawing.Point(674, 7);
+            this.btnSearch.Name = "btnSearch";
+            this.btnSearch.Size = new System.Drawing.Size(52, 23);
+            this.btnSearch.TabIndex = 5;
+            this.btnSearch.Text = "GO";
+            this.btnSearch.UseVisualStyleBackColor = true;
+            this.btnSearch.Click += new System.EventHandler(this.btnSearchPressed);
+            // 
+            // htmlTextBox
+            // 
+            this.htmlTextBox.Location = new System.Drawing.Point(0, 36);
+            this.htmlTextBox.Multiline = true;
+            this.htmlTextBox.Name = "htmlTextBox";
+            this.htmlTextBox.Size = new System.Drawing.Size(984, 497);
+            this.htmlTextBox.TabIndex = 6;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(984, 561);
+            this.Controls.Add(this.htmlTextBox);
+            this.Controls.Add(this.btnSearch);
+            this.Controls.Add(this.btnRefresh);
+            this.Controls.Add(this.btnForward);
+            this.Controls.Add(this.btnBack);
+            this.Controls.Add(this.searchBar);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+        private System.Windows.Forms.TextBox searchBar;
+        private System.Windows.Forms.Button btnBack;
+        private System.Windows.Forms.Button btnForward;
+        private System.Windows.Forms.Button btnRefresh;
+        private System.Windows.Forms.Button btnSearch;
+        private System.Windows.Forms.TextBox htmlTextBox;
     }
 }
 
--- BrowserSolution/WebBrowser/Form1.cs
+++ BrowserSolution/WebBrowser/Form1.cs
@@ -7,14 +7,92 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Net.Http;
+using System.Collections;
 
 namespace WebBrowser
 {
     public partial class Form1 : Form
     {
+        static Stack<String> backStack = new Stack<String>();
+        static Stack<String> forwardStack = new Stack<String>();
+        static String currentPageAddress = "";
+
         public Form1()
         {
             InitializeComponent();
         }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnSearchPressed(object sender, EventArgs e)
+        {
+            backStack.Push(currentPageAddress);
+            String address = searchBar.Text;
+            Search(address);
+        }
+
+        private void Refresh(object sender, EventArgs e)
+        {
+            Search(currentPageAddress);
+        }
+
+        private void Back(object sender, EventArgs e)
+        {
+            if (backStack.Count != 0)
+            {
+                forwardStack.Push(currentPageAddress);
+                currentPageAddress = backStack.Pop();
+                Search(currentPageAddress);
+            } else
+            {
+                Console.WriteLine("No pages to go backwards to");
+            } //Was going to use a try catch but then theres no way to be sure of poppable backstack, so would would current page address an then fail at pop of empty back stack
+            
+        }
+
+        private void Forward(object sender, EventArgs e)
+        {
+            if (forwardStack.Count != 0)
+            {
+                backStack.Push(currentPageAddress);
+                currentPageAddress = forwardStack.Pop();
+                Search(currentPageAddress);
+            }
+            else
+            {
+                Console.WriteLine("No pages to go forward to");
+            }        
+          
+        }
+
+        // HttpClient is intended to be instantiated once per application, rather than per-use. See Remarks.
+        static readonly HttpClient client = new HttpClient();
+
+        async Task Search(String address)
+        {
+            // Call asynchronous network methods in a try/catch block to handle exceptions.
+            try
+            {
+                HttpResponseMessage response = await client.GetAsync(address);
+                response.EnsureSuccessStatusCode();
+                string responseBody = await response.Content.ReadAsStringAsync();
+                // Above three lines can be replaced with new helper method below
+                // string responseBody = await client.GetStringAsync(uri);
+                
+                htmlTextBox.Text = responseBody;
+                currentPageAddress = address;
+            }
+            catch (HttpRequestException e)
+            {
+                Console.WriteLine("\nException Caught!");
+                Console.WriteLine("Message :{0} ", e.Message);
+            }
+        }
+
     }
-}
+
+    }
